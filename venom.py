491df2a990140fd4a878fc077edf5ccacb158dab
RoryGarner92 <rorygarner92@gmail.com>
2017-10-19 15:36:23 +0100

    added comments, removed pprint import

--- venom.py
+++ venom.py
@@ -1,8 +1,13 @@
+#Rory Garner
+#C00193506
+#IT Carlow
+#Software Development
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import pprint
+
 #content = urlopen("http://itcarlow.ie")
 content = urlopen("http://testing-ground.scraping.pro/blocks")
 soup = BeautifulSoup(content.read(),"lxml")
-print(soup.h1)
+print(soup.find_all())
 #print(content.read())
